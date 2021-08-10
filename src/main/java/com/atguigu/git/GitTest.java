1789471885a853a75bddde7d1f38e9ee2f6ca322
white_glb <helloworld_glb@white.com>
2021-08-10 09:29:58 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @Author: Shun
+ * @Date: 2021/8/10 9:25
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
